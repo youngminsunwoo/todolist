43d282e40affe0eb3bfe782bd0d35fdfa8ed8280
william.lacy <williamlacy@uk.ibm.com>
2016-08-10 16:38:47 +0100

    adding filter e2es

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -11,6 +11,11 @@ var MainPage = function() {
   this.newToDoFormEl = element(by.css('.todo-form'));
   this.newToDoInputEl = this.newToDoFormEl.element(by.css('.new-todo'));
   this.todoEls = element.all(by.repeater('todo in filterByStatus(todos)'))
+  this.todoStatusFilterEl = element(by.css('.filters'));
+  this.todoAllStatusFilterEl = this.todoStatusFilterEl.element(by.css('.do-all'));
+  this.todoCompletedStatusFilterEl = this.todoStatusFilterEl.element(by.css('.do-completed'));
+  this.todoActiveStatusFilterEl = this.todoStatusFilterEl.element(by.css('.do-active'));
+
 };
 
 module.exports = new MainPage();
--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -85,16 +85,33 @@ describe('Main View', function () {
     });
 
     describe('on filtering todos', function () {
-      it('should not filter any todos when filtering by "All"', function () {
-
+      beforeEach(function(){
+        page.newToDoInputEl.sendKeys('Buy a spaceship');
+        page.newToDoInputEl.sendKeys(protractor.Key.ENTER);
+        page.newToDoInputEl.sendKeys('Enjoy the DevOps course');
+        page.newToDoInputEl.sendKeys(protractor.Key.ENTER);
+        page.todoEls.get(1).element(by.css('.toggle')).click(); // toggle already complete
+        page.newToDoInputEl.sendKeys('Tell friends and family about the course!');
+        page.newToDoInputEl.sendKeys(protractor.Key.ENTER);
       });
 
       it('should filter on not completed todos when filtering by "Active"', function () {
+        expect(page.todoEls.count()).toBe(3);
+        page.todoActiveStatusFilterEl.click();
+        expect(page.todoEls.count()).toBe(2);
+      });
 
+      it('should not filter any todos when filtering by "All"', function () {
+        page.todoActiveStatusFilterEl.click();// default page load is all, navigate away to prove the test
+        expect(page.todoEls.count()).toBe(2);
+        page.todoAllStatusFilterEl.click();
+        expect(page.todoEls.count()).toBe(3);
       });
 
       it('should filter on completed todos when filtering by "Completed"', function () {
-
+        expect(page.todoEls.count()).toBe(3);
+        page.todoCompletedStatusFilterEl.click();
+        expect(page.todoEls.count()).toBe(1);
       });
     });
   });
