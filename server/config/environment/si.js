43717337801040a939b66835c1c76498a0c1d71d
Donal Spring <donalspring@gmail.com>
2016-08-12 11:39:59 +0100

    Mongo container working

--- server/config/environment/si.js
+++ server/config/environment/si.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/todolist-si'
+    uri: 'mongodb://mongo/todolist-si'
   },
   seedDB: true
 };
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/todolist-test'
+    uri: 'mongodb://docker.local/todolist-ci'
   },
   seedDB: true
 };
