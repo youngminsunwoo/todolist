2204941b55fe47692b5782ff3648fc281dbc9ea6
William Lacy <devops@devops.com>
2016-08-15 15:46:46 +0100

    add git config to jenkins user so it does not fail builds

--- puppet/build-vm.pp
+++ puppet/build-vm.pp
@@ -99,7 +99,7 @@ user { 'jenkins' :
 } ->
 
 exec { 'gitconfig':
-	command => "git config --global user.email \"jenkins@jenkins.ci\" && git config --global user.name \"jenkins\"'",
+	command => "/usr/bin/git config --global user.email \"jenkins@jenkins.ci\" && /usr/bin/git config --global user.name \"jenkins\"'",
 	user    => "jenkins"
 }
 
