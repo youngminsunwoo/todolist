768879ca6a7b26b456d3f3ad28afe13cc7f1a372
william.lacy <williamlacy@uk.ibm.com>
2016-08-15 15:36:29 +0100

    fixing dodgy sudo command

--- puppet/build-vm.pp
+++ puppet/build-vm.pp
@@ -99,7 +99,7 @@ user { 'jenkins' :
 } ->
 
 exec { 'gitconfig':
-	command => "sudo -u jenkins /bin/bash -c 'git config --global user.email \"jenkins@jenkins.ci\" && git config --global user.name \"jenkins\"'",
+	command => "sh jenkins -c 'git config --global user.email \"jenkins@jenkins.ci\" && git config --global user.name \"jenkins\"'",
 }
 
 exec { 'keygen':
