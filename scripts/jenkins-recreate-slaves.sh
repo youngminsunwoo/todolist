b7b194047afb341a604de644cdbca4a16226093e
William Lacy <devops@devops.com>
2016-08-15 13:41:19 +0100

    Derp

--- scripts/jenkins-recreate-slaves.sh
+++ scripts/jenkins-recreate-slaves.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
-# Restarts the container passed in 
+# stops and removes containers matching $image_name-
+# reruns number of slaves with port range starting template_port+1
 
 # # run ::
-# chmod +x ./scripts/jenkins-restart-app.sh
-# eg ./scripts/jenkins-restart-app.sh todo-app-ci todolist-build:12
+# chmod +x ./scripts/jenkins-recreate-slaves.sh
+# ./scripts/jenkins-recreate-slaves.sh jenkins-bluemix-slave 2210 5
 
-environ=${1}
-tag=${2}
-
-if [[ -z ${environ} || -z ${tag} ]]
- then echo "no param passed to script, must provide enviroment and tag"
+image_name=${1}; shift
+template_port=${1}; shift
+number_of_slaves=${1}
+if [[ -z ${image_name} || -z ${template_port} || -z ${number_of_slaves} ]]
+ then echo "no param passed to script, must atleast provide a base image name"
  exit 1;
 fi
 
-echo "DOCKER STOPPING CONTAINER " todo-app-${environ}
-docker stop todo-app-${environ} && rc=$?
+echo "tearing down containers"
+for slave in `docker ps | grep "${image_name}-" | awk -F' ' '{ print $1 }'`
+ do
+    echo "stopping $slave"
+    docker stop $slave
+ done
+for slave in `docker ps -a | grep "${image_name}-" | awk -F' ' '{ print $1 }'`
+ do
+    echo "removing $slave"
+    docker rm $slave
+ done
 
-echo "DOCKER REMOVING CONTAINER " todo-app-${environ}
-docker rm todo-app-${environ} && rc=$?
+echo "determining the docker host net address from /etc/hosts"
+docker_host=$(cat /etc/hosts | egrep -o '^[[:space:]]*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}[[:space:]]+docker.local[[:space:]]*$' | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 
-
-echo "DOCKER STARTING CONTAINER " todo-app-${environ}
-if [ "$environ" == "ci" ]; then
-  port=9001
-  docker run -t -d --name todo-app-${environ} -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}
-elif [ "$environ" == "si" ]; then
-  port=9002
-  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
-elif [ "$environ" == "production" ]; then
-  port=80
-  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
-else 
-  echo "ENV not set"
-  exit 1;
+if [[ -z ${docker_host} ]]
+ then echo 'ERROR docker host address not found in /etc/hosts'
+ exit 1
 fi
+
+port=$((template_port))
+while [ ${port} -le $((template_port+number_of_slaves)) ]
+ do
+   echo "running docker container ${image_name}-${port}"
+   docker run -p ${port}:22 -t -i -d --name ${image_name}-${port} \
+    	--add-host="docker.host:${docker_host}" \
+    	--add-host="git.server:${docker_host}" \
+    	--add-host="mongo.server:${docker_host}" \
+    	--add-host="jenkins.server:${docker_host}" \
+    	${image_name}
+   		port=$((port+1))
+ done
--- scripts/jenkins-restart-app.sh
+++ scripts/jenkins-restart-app.sh
@@ -5,28 +5,32 @@
 # chmod +x ./scripts/jenkins-restart-app.sh
 # eg ./scripts/jenkins-restart-app.sh todo-app-ci todolist-build:12
 
-env=${1}
+environ=${1}
 tag=${2}
 
-if [[ -z ${env} || -z ${tag} ]]
+if [[ -z ${environ} || -z ${tag} ]]
  then echo "no param passed to script, must provide enviroment and tag"
  exit 1;
 fi
 
-echo "DOCKER STOPPING CONTAINER " todo-app-${env}
-docker stop todo-app-${env} && rc=$?
+echo "DOCKER STOPPING CONTAINER " todo-app-${environ}
+docker stop todo-app-${environ} && rc=$?
 
-echo "\nDOCKER REMOVING CONTAINER " todo-app-${env}
-docker rm todo-app-${env} && rc=$?
+echo "DOCKER REMOVING CONTAINER " todo-app-${environ}
+docker rm todo-app-${environ} && rc=$?
 
 
-echo "\nDOCKER STARTING CONTAINER " todo-app-${env}
-if [ "$env" == "ci" ]; then
-	port=9001
-	docker run -t -d --name todo-app-${env} -p ${port}:9000 --env NODE_ENV=${env} todo-app:${tag}
-elif [ "$env" == "si" ]; then
-	port=9002
-	docker run -t -d --name todo-app-${env} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${env} todo-app:${tag}	
-elif [ "$env" == "production" ]; then
-	port=80
-	docker run -t -d --name todo-app-${env} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${env} todo-app:${tag}	
+echo "DOCKER STARTING CONTAINER " todo-app-${environ}
+if [ "$environ" == "ci" ]; then
+  port=9001
+  docker run -t -d --name todo-app-${environ} -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}
+elif [ "$environ" == "si" ]; then
+  port=9002
+  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
+elif [ "$environ" == "production" ]; then
+  port=80
+  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
+else 
+  echo "ENV not set"
+  exit 1;
+fi
