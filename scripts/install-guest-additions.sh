3f2eabe61417a08a284bc66e6d3a3bd1476cfdc1
william.lacy <williamlacy@uk.ibm.com>
2016-08-22 12:45:19 +0100

    update headlessly

--- scripts/install-guest-additions.sh
+++ scripts/install-guest-additions.sh
@@ -2,6 +2,7 @@
 # mounts and install guest additions
 mkdir -p /mnt/guest-additions
 mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/guest-additions
-/mnt/guest-additions/VBoxLinuxAdditions.run
+/mnt/guest-additions/runasroot.sh
 mkdir -p /share/.puppet-breadcrumbs
 touch /share/.puppet-breadcrumbs/install-guest-additions
+umount /mnt/guest-additions
