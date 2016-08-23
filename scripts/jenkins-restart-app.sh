9410789af8235d6d8f221f455dd571bfad6830bd
william.lacy <williamlacy@uk.ibm.com>
2016-08-23 13:33:01 +0100

    rename todo-app to todolist

--- scripts/jenkins-restart-app.sh
+++ scripts/jenkins-restart-app.sh
@@ -3,7 +3,7 @@
 
 # # run ::
 # chmod +x ./scripts/jenkins-restart-app.sh
-# eg ./scripts/jenkins-restart-app.sh todo-app-ci todolist-build:12
+# eg ./scripts/jenkins-restart-app.sh todolist-ci todolist-build:12
 
 environ=${1}
 tag=${2}
@@ -13,23 +13,23 @@ if [[ -z ${environ} || -z ${tag} ]]
  exit 1;
 fi
 
-echo "DOCKER STOPPING CONTAINER " todo-app-${environ}
-docker stop todo-app-${environ} && rc=$?
+echo "DOCKER STOPPING CONTAINER " todolist-${environ}
+docker stop todolist-${environ} && rc=$?
 
-echo "DOCKER REMOVING CONTAINER " todo-app-${environ}
-docker rm todo-app-${environ} && rc=$?
+echo "DOCKER REMOVING CONTAINER " todolist-${environ}
+docker rm todolist-${environ} && rc=$?
 
 
-echo "DOCKER STARTING CONTAINER " todo-app-${environ}
+echo "DOCKER STARTING CONTAINER " todolist-${environ}
 if [ "$environ" == "ci" ]; then
   port=9001
-  docker run -t -d --name todo-app-${environ} -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}
+  docker run -t -d --name todolist-${environ} -p ${port}:9000 --env NODE_ENV=${environ} todolist:${tag}
 elif [ "$environ" == "si" ]; then
   port=9002
-  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
+  docker run -t -d --name todolist-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todolist:${tag}  
 elif [ "$environ" == "production" ]; then
   port=80
-  docker run -t -d --name todo-app-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todo-app:${tag}  
+  docker run -t -d --name todolist-${environ} --link devops-mongo:mongo  -p ${port}:9000 --env NODE_ENV=${environ} todolist:${tag}  
 else 
   echo "ENV not set"
   exit 1;
