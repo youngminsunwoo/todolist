54a8cdaa460a5f1bb79cbe1758c578ad3e1db60f
William Lacy <williamlacy@uk.ibm.com>
2016-12-21 10:48:33 +0000

    Remove vbox guest additions install as it is part of the instructions

--- puppet/build-vm.pp
+++ puppet/build-vm.pp
@@ -71,16 +71,6 @@ package { ['vim', 'curl', 'wget', 'openssh-client', 'openssh-server', 'git-core'
   ensure => present,
 }
 
-# guest additions
-package { 'virtualbox-guest-additions-iso':
-  ensure => present
-} ->
-
-exec {'install guest additions':
-  command => "/bin/bash -c 'sudo ../scripts/install-guest-additions.sh'",
-  creates => "/share/.puppet-breadcrumbs/install-guest-additions"
-}
-
 # nodejs install...
 
 # Jenkins install...
