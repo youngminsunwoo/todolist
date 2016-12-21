06532fb676ddea18e632e10d939e5a4f4122c787
William Lacy <williamlacy@uk.ibm.com>
2016-12-21 10:49:51 +0000

    remove guest additions install as it slowed down puppet runs significantly slowly

--- scripts/install-guest-additions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# mounts and install guest additions
-mkdir -p /mnt/guest-additions
-mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/guest-additions
-/mnt/guest-additions/VBoxLinuxAdditions.run --nox11
-mkdir -p /share/.puppet-breadcrumbs
-touch /share/.puppet-breadcrumbs/install-guest-additions
-umount /mnt/guest-additions
